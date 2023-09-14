9c6ea88245a7d9b3c232e9f7158101d4d241a68d
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-14 10:38:27 -0300

    added store/index.js

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import reducers from './reducers'
+
+
+export const initialState = {
+    other: '...',
+    cart: [],
+    products: [],
+    user: null,
+    number: 0
+}
+
+export {
+    reducers,
+    initialState
+}
