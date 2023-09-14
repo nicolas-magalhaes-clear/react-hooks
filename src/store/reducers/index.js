8cbe21658bb6d3a7a945d6bc9460306c325973cf
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-14 10:38:59 -0300

    added: user.js

---
+++ src/store/reducers/index.js
@@ -0,0 +1,19 @@
+export default function reducer(state, action) {
+
+    switch (action.type) {
+
+        case 'number_add2':
+            return ({ ...state, number: state.number + 2 })
+        case 'login':
+            return ({ ...state, user: { name: action.payload } })
+        case 'numberDiv25':
+            return ({...state, number: state.number / 25})
+        case 'numberInt':
+            return({...state, number: parseInt(state.number)})
+        case 'addNumberN':
+            return({...state, number: state.number + action.payload})
+        default:
+            return state
+    }
+
+}
